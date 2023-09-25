fc96e4c4ac295c4d3a4aee917c3c60750590cbb7
Ringo De Smet <ringo@de-smet.name>
2023-09-25 08:39:09 +0200

    Spawn the process for the dnsplugin
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- provider/cmd/pulumi-resource-acme/main.go
+++ provider/cmd/pulumi-resource-acme/main.go
@@ -18,16 +18,22 @@ package main
 
 import (
 	_ "embed"
+	"os"
 
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	acme "github.com/pulumiverse/pulumi-acme/provider"
 	"github.com/pulumiverse/pulumi-acme/provider/pkg/version"
+	"github.com/vancluever/terraform-provider-acme/v2/acme/dnsplugin"
 )
 
 //go:embed schema-embed.json
 var pulumiSchema []byte
 
 func main() {
-	// Modify the path to point to the new provider
-	tfbridge.Main("acme", version.Version, acme.Provider(), pulumiSchema)
+	if len(os.Args) == 2 && os.Args[1] == dnsplugin.PluginArg {
+		// Start the plugin here
+		dnsplugin.Serve()
+	} else {
+		tfbridge.Main("acme", version.Version, acme.Provider(), pulumiSchema)
+	}
 }
